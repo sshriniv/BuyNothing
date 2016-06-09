86ea6a8d42445fd0f2d1274c1ed560e03a322e7d
Sandeep <sandeep.shri@gmail.com>
2016-06-09 00:39:09 +0000

    home page ui changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,12 @@
 class PagesController < ApplicationController
   def home
-    @current_user_groups = current_user.groups
+    if current_user
+      @current_user_groups = current_user.groups
+      
+      @current_user_groups_posts = 0
+      @current_user_groups.each do |current_user_group|
+        @current_user_groups_posts = @current_user_groups_posts+current_user_group.posts.count
+      end
+    end
   end
 end
